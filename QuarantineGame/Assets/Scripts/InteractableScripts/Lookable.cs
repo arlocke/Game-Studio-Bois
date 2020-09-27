482ded7c4be2422142c5e693e8ab7e307ca45836
Elrik Larson <elriklarson.design@gmail.com>
2020-09-27 18:58:26 -0400

    Lookable Adjusted
    Lookable now rotates the object apropriately on picking up and putting down.

--- QuarantineGame/Assets/Scripts/InteractableScripts/Lookable.cs
+++ QuarantineGame/Assets/Scripts/InteractableScripts/Lookable.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Lookable : MonoBehaviour
 {
     public float distanceFromCamera = 2.0f;
+    public float dSpeed = 1.0f;
+    public float rSpeed = 1.0f;
     public bool isHit = false;
 
     protected bool beingCarried = false;
     protected bool touched = false;
+    protected bool arrived = false;
+    protected bool snapped = false;
 
     protected Vector3 lastPosition;
     protected Vector3 lastRotation;
@@ -23,6 +27,18 @@ public class Lookable : MonoBehaviour
         if(self == null)
         {
             Debug.Log("Self can't find Collider - Lookable");
+        }
+    }
+
+    private void Update()
+    {
+        MoveObject();
+        RotateObject();
+        if(!arrived && !snapped && !beingCarried)
+        {
+            cameraLook.enabled = true;
+            player.enabled = true;
+            self.enabled = true;
         }
     }
 
@@ -53,7 +69,6 @@ public class Lookable : MonoBehaviour
                 player.enabled = false;
             }
             self.enabled = false;
-            transform.position = carrier.position + (carrier.forward * distanceFromCamera);
         }
         return isHit;
     }
@@ -62,12 +77,51 @@ public class Lookable : MonoBehaviour
     {
         Debug.Log("Dropping");
         transform.parent = null;
-        transform.position = lastPosition;
-        transform.eulerAngles = lastRotation;
+        //transform.position = lastPosition;
+        //transform.eulerAngles = lastRotation;
         beingCarried = false;
         carrier = null;
-        cameraLook.enabled = true;
-        player.enabled = true;
-        self.enabled = true;
+    }
+
+    private void MoveObject()
+    {
+        if (beingCarried && !arrived)
+        {
+            var target = carrier.position + (carrier.forward * distanceFromCamera);
+            transform.position = Vector3.MoveTowards(transform.position, target, dSpeed * Time.deltaTime);
+            if (transform.position == target)
+            {
+                arrived = true;
+            }
+        }
+        else if (!beingCarried && arrived)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, lastPosition, dSpeed * Time.deltaTime);
+            if (transform.position == lastPosition)
+            {
+                arrived = false;
+                
+            }
+        }
+    }
+
+    private void RotateObject()
+    {
+        if(beingCarried && !snapped)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, carrier.rotation, rSpeed);
+            if(transform.rotation == carrier.rotation)
+            {
+                snapped = true;
+            }
+        }
+        else if(!beingCarried && snapped)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(lastRotation), rSpeed);
+            if(transform.rotation == Quaternion.Euler(lastRotation))
+            {
+                snapped = false;
+            }
+        }
     }
 }
