19f2ab5a8021f109e58a1b14126e6ac0d06eda96
arlocke <alocke692@gmail.com>
2020-11-07 03:09:32 -0500

    1st repair push

--- QuarantineGame/Assets/_Scripts/Clock.cs
+++ QuarantineGame/Assets/_Scripts/Clock.cs
@@ -22,6 +22,20 @@ public class Clock : MonoBehaviour
 
     float lastTime = 0.0f;
 
+    /// The Many Bools
+
+    bool Thought1 = false;
+    bool Thought2 = false;
+    bool Thought3 = false;
+    bool Thought4 = false;
+    bool Thought5 = false;
+
+    bool Email1 = false;
+    bool Email2 = false;
+    bool Email3 = false;
+
+    /// 
+
 
     // Start is called before the first frame update
     void Start()
@@ -49,42 +63,69 @@ public class Clock : MonoBehaviour
              clocksToDisplay[i].text = hoursString + ":" + minutesString; // is this bad?
         }
 
-        if(timeRounded == 455)
-        {
-            if(lastTime < 454)
-            {
-                lastTime = time;
-            }
-            if(lastTime == time)
-            {
-                Debug.Log("First Thought");
-                EventManager.OnInnerThoughtInitiated("hello I want to do this", 5.0f);
-            }
-        }
 
-        if(timeRounded == 465)
-        {
-            if(lastTime < 464)
+            if (timeRounded == 455 && !Thought1)
             {
-                lastTime = time;
+                EventManager.OnInnerThoughtInitiated("hello I want to do this", 10.0f);
+                Thought1 = true;
             }
-            if(lastTime == time)
-            {
-                Debug.Log("Second Thought");
-                EventManager.OnInnerThoughtInitiated("this is the second thing Im thinking of", 5.0f);
-            }
-        }
 
-        if(timeRounded == 480)
-        {
-            if(lastTime < 480)
+            if (timeRounded == 470 && !Thought2)
             {
-                lastTime = time;
+                EventManager.OnInnerThoughtInitiated("This is my second thought ever!", 10.0f);
+                Thought2 = true;
             }
-            if(lastTime == time)
+
+            if (timeRounded == 460 && !Email1)
             {
-                EventManager.OnAddEmailInitiated("What is up ladies and gentleman");
+                EventManager.OnAddEmailInitiated("Sender: The Boss \n \n Hey Gahara! \n Make sure you attend the remote work meeting today." +
+                    " I'm counting on you to be the star employee to show the other guys we can transition to remote working smoothly. Don't let me down! \n -The Boss");
+                Email1 = true;
             }
-        }
+
+            if (timeRounded == 465 && !Email2)
+            {
+                EventManager.OnAddEmailInitiated("Sender: The Boss \n \n Hey Gahara! \n Also FYI, this is just a second email reminder \n -The Boss");
+                Email2 = true;
+            }
+
+            /*
+            if(timeRounded == 455)
+            {
+                if(lastTime < 454)
+                {
+                    lastTime = time;
+                }
+                if(lastTime == time)
+                {
+                    Debug.Log("First Thought");
+                    EventManager.OnInnerThoughtInitiated("hello I want to do this", 5.0f);
+                }
+            }
+
+            if(timeRounded == 465)
+            {
+                if(lastTime < 464)
+                {
+                    lastTime = time;
+                }
+                if(lastTime == time)
+                {
+                    Debug.Log("Second Thought");
+                    EventManager.OnInnerThoughtInitiated("this is the second thing Im thinking of", 5.0f);
+                }
+            }
+
+            if(timeRounded == 480)
+            {
+                if(lastTime < 480)
+                {
+                    lastTime = time;
+                }
+                if(lastTime == time)
+                {
+                    EventManager.OnAddEmailInitiated("What is up ladies and gentleman");
+                }
+            }*/
     }
 }
--- QuarantineGame/Assets/_Scripts/GameManager.cs
+++ QuarantineGame/Assets/_Scripts/GameManager.cs
@@ -2,20 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
-
-    public bool endGame = false;
-    public float restartDelay = 3f;
     public GameObject playerCam;
 
-    public Text innerThoughtsUI; //This is bad programming
 
     void Start()
     {
-        innerThoughtsUI.text = ""; //This is bad programming
         if (playerCam == null)
         {
             playerCam = GameObject.FindGameObjectWithTag("MainCamera"); //Right now havePills is in playerRaycast - maybe make a script in the future for storing playerData 
@@ -27,24 +22,13 @@ public class GameManager : MonoBehaviour
     {
         if(playerCam.GetComponent<PlayerRaycast>().havePills == true) 
         {
-            innerThoughtsUI.text = "DAY COMPLETED";
+            
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // THIS IS SO FUCKING BAD CODING
         }
         else
         {
-            innerThoughtsUI.text = "I need to take my pills first...";
+            EventManager.OnInnerThoughtInitiated("I need to take my pills first...", 10.0f);
         } 
-    }
-
-
-    public void EndGame()
-    {
-        if (endGame == false)
-        {
-            endGame = true;
-            Debug.Log("Game Over");
-            //Invoke("RestartGame", restartDelay);
-        }
     }
 
 }
