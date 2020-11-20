224dd3931f2e5c1d8d365be16d466cde8329135b
Elrik Larson <elriklarson.design@gmail.com>
2020-11-20 14:27:17 -0500

    ....
    more code movement

--- QuarantineGame/Assets/_Scripts/Managers/_DayManagers/Day1.cs
+++ QuarantineGame/Assets/_Scripts/Managers/_DayManagers/Day1.cs
@@ -6,15 +6,97 @@ public class Day1 : MonoBehaviour
 {
     public Clock clock;
 
-    public float time;
+    private float time;
+    bool Thought1 = false;
+    bool Thought2 = false;
+    bool Thought3 = false;
+    bool Thought4 = false;
+    bool Thought5 = false;
+    bool Thought6 = false;
+    bool Thought7 = false;
+    bool Thought8 = false;
+    bool Thought9 = false;
+    bool Thought10 = false;
+    bool Thought11 = false;
+    bool Thought12 = false;
+    bool Thought13 = false;
+
+    bool Email1 = false;
+    bool Email2 = false;
+    bool Email3 = false;
+
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
-        if (time >= 440)
+        time = clock.timeRounded;
+        if (time == 455 && !Thought1)
         {
-            EventManager.OnInnerThoughtInitiated("I should check my bulletin board", 10.0f);
+            Debug.Log("Doing 1st thought");
+            EventManager.OnInnerThoughtInitiated("Ugh I'm so tired... I gotta get outta bed... (Use [LeftCtrl] to get up)", 7.0f);
+            Thought1 = true;
+        }
+
+        if (time == 465 && !Thought2)
+        {
+            Debug.Log("Doing 1st thought");
+            EventManager.OnInnerThoughtInitiated("Time to get a move on... (Use WASD to walk around)", 7.0f);
+            Thought2 = true;
+        }
+
+        if (time == 480 && !Thought3)
+        {
+            EventManager.OnInnerThoughtInitiated("I better check my bulletin board for my daily tasks... (Click on Sticky Notes to receive quests)", 10.0f);
+            Thought3 = true;
+        }
+
+        if (time == 500 && !Thought4)
+        {
+            EventManager.OnInnerThoughtInitiated("Oh jeez... My boss must've emailed me about my morning meeting...", 10.0f);
+            Thought4 = true;
+        }
+
+        if (time == 500 && !Email1)
+        {
+            EventManager.OnAddEmailInitiated("Sender: The Boss \n \n Hey Gahara! \n Make sure you attend the remote work meeting today." +
+                " I'm counting on you to be the star employee to show the other guys we can transition to remote working smoothly. Don't let me down! \n -The Boss");
+            Email1 = true;
+        }
+
+        if (time == 530 && !Email2)
+        {
+            EventManager.OnAddEmailInitiated("Sender: The Boss \n \n Hey Gahara! \n Also FYI... Don't mention anything that about the virus in the meeting... We have to keep morale up and I know you're someone who easily goes off the rails. Don't let me down! \n -The Boss");
+            Email2 = true;
+        }
+
+        if (time == 530 && !Thought5)
+        {
+            EventManager.OnInnerThoughtInitiated("This guy emailed me again!? What a psycho...", 10.0f);
+            Thought5 = true;
+        }
+
+        if (time == 600 && !Thought6)
+        {
+            EventManager.OnInnerThoughtInitiated("I've done my work for the day! I just need to remember to take my pills before I turn in.", 10.0f);
+            Thought6 = true;
+        }
+
+        if (time == 650 && !Email3)
+        {
+            EventManager.OnAddEmailInitiated("Sender: Doc \n \n Hello Mr. Gahara! \n Thanks for participating in our clinical vaccine trial. Your help will go a long way towards beating this pandemic together. We're counting on you! \n -Your friendly neighborhood doctor");
+            Email3 = true;
+        }
+
+        if (time == 650 && !Thought7)
+        {
+            EventManager.OnInnerThoughtInitiated("Looks like I got an email from my doctor. Wonder what he wanted to say...", 10.0f);
+            Thought7 = true;
+        }
+
+        if (time == 700 && !Thought8)
+        {
+            EventManager.OnInnerThoughtInitiated("Wow I'm still so tired... I could hit the hay right now!", 10.0f);
+            Thought8 = true;
         }
 
     }
