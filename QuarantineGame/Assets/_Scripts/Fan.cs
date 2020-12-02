31e45dbb5605e14dfbad3d89a7a0773a347193c0
arlocke <alocke692@gmail.com>
2020-12-02 04:18:09 -0500

    lighting and doors

---
+++ QuarantineGame/Assets/_Scripts/Fan.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    public float spinSpeed = 50.0f;
+
+    private void Update()
+    {
+        transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+    }
+}
+
