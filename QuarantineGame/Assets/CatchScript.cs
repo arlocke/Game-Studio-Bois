a11792133f5ac71ed208d42ac76dfbff5e612b4d
Elrik Larson <elriklarson.design@gmail.com>
2020-12-14 01:21:30 -0500

    Basic Catch Volume
    Can't solve everything going through the floor so might as well catch them while they are falling.

---
+++ QuarantineGame/Assets/CatchScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchScript : MonoBehaviour
+{
+    public Transform position;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody dud = other.transform.GetComponent<Rigidbody>();
+        if(dud != null)
+        {
+            dud.velocity = Vector3.zero;
+        }
+        other.transform.position = position.position;
+    }
+}
