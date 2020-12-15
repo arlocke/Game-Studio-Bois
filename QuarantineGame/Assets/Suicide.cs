e39b4078fa43602e8aa8eb27a4cb1f9626695d24
arlocke <alocke692@gmail.com>
2020-12-15 15:44:56 -0500

    Final Individual Push for now

---
+++ QuarantineGame/Assets/Suicide.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suicide : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ QuarantineGame/Assets/_Scripts/TakeVaccinePrompt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TakeVaccinePrompt : MonoBehaviour
+{
+    public GameManager gameManager;
+    public CanvasGroup self;
+
+    public void Yes()
+    {
+        self.alpha = 0;
+        self.blocksRaycasts = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        gameManager.CompleteDay();
+    }
+
+    public void No()
+    {
+        self.alpha = 0;
+        self.blocksRaycasts = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Open()
+    {
+        self.alpha = 1;
+        self.blocksRaycasts = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
