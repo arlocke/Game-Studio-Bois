5a4d8432d8a8e2587454d7bb337c4f455c91f4e0
Elrik Larson <elriklarson.design@gmail.com>
2020-12-15 18:47:01 -0500

    MoRe SmOoThInG

---
+++ QuarantineGame/Assets/_Scripts/Days/Day6.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day6 : MonoBehaviour
+{
+    public Clock clock;
+
+    private void Start()
+    {
+        clock.time += 660;
+    }
+}
