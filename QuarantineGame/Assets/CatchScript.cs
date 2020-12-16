100f7940a2276df701a00d99659afa729eb5ecf0
Elrik Larson <elriklarson.design@gmail.com>
2020-12-16 13:19:45 -0500

    Catching is Fine

--- QuarantineGame/Assets/CatchScript.cs
+++ QuarantineGame/Assets/CatchScript.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class CatchScript : MonoBehaviour
 {
     public Transform position;
+    public Transform position2;
+    public bool Day5;
 
     private void OnTriggerEnter(Collider other)
     {
         Rigidbody dud = other.transform.GetComponent<Rigidbody>();
-        if(dud != null)
+        if (Day5)
         {
-            dud.velocity = Vector3.zero;
+            if(other.name == "Crowbar")
+            {
+                if (dud != null)
+                {
+                    dud.velocity = Vector3.zero;
+                }
+                other.transform.position = position2.position;
+            }
+            else
+            {
+                if (dud != null)
+                {
+                    dud.velocity = Vector3.zero;
+                }
+                other.transform.position = position.position;
+            }
         }
-        other.transform.position = position.position;
+        else
+        {
+            if (dud != null)
+            {
+                dud.velocity = Vector3.zero;
+            }
+            other.transform.position = position.position;
+        }
     }
 }
