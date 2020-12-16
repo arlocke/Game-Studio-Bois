bf5051174fd2bc22a4acb5a71acb7c6ca2f5ff13
Elrik Larson <elriklarson.design@gmail.com>
2020-12-16 04:50:28 -0500

    Day 5 Path Corrected
    Back to original path as snapping feature is implimented.

---
+++ QuarantineGame/Assets/_Scripts/Day5SnapVolume.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day5SnapVolume : MonoBehaviour
+{
+    public GameObject wood1;
+    public GameObject wood2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Wood" && wood1 != null && wood2 != null)
+        {
+            if(!wood1.activeSelf)
+            {
+                wood1.SetActive(true);
+            }
+            else if(!wood2.activeSelf)
+            {
+                wood2.SetActive(true);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+            other.gameObject.SetActive(false);
+        }
+    }
+}
