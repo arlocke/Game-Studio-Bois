08669f7faba08e792561d603ae7444cfdbf3404f
Elrik Larson <elriklarson.design@gmail.com>
2020-12-16 04:26:45 -0500

    Maildoor in day 5 stays closed
    So if you put the dna in the mail door for day 5 AND have taken your pills. Then the door will stay closed when you lift it up! Neat.

---
+++ QuarantineGame/Assets/_Scripts/Day5MailDoorLocked.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day5MailDoorLocked : MonoBehaviour
+{
+    public Transform MailDoor;
+    public Rigidbody MailDoorPhysics;
+    public string key = "";
+
+    private void OnTriggerStay(Collider other)
+    {
+        var dud = other.GetComponent<QuestObjective>();
+        var name = EventManager.NameFromLoader(key);
+        if (dud != null && name != null)
+        {
+            if (dud.key == key && EventManager.OnQuestCheck(name) && EventManager.OnQuestCheck("Pills"))
+            {
+                if(MailDoor.eulerAngles.x <= 0.5f)
+                {
+                    MailDoorPhysics.isKinematic = true;
+                }
+            }
+        }
+    }
+}
